28a6ca8a10545dff788c4e51dd328c26dfff886a
Bip901 <74911767+Bip901@users.noreply.github.com>
2024-01-05 22:00:05 +0200

    Restore original focus

--- AvaloniaDialogs/Views/BaseDialog.cs
+++ AvaloniaDialogs/Views/BaseDialog.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Data;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.LogicalTree;
 using DialogHostAvalonia;
 using System.Threading.Tasks;
@@ -13,12 +14,43 @@ namespace AvaloniaDialogs.Views;
 /// </summary>
 public abstract class BaseDialog : UserControl
 {
+    private IInputElement? previousFocus;
+
     /// <summary>
     /// Called before closing this dialog. Return false to cancel.
     /// </summary>
     public virtual bool OnClosing()
     {
         return true;
+    }
+
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        base.OnUnloaded(e);
+        previousFocus?.Focus();
+    }
+
+    public virtual void Close()
+    {
+        DialogHost.Close(null, null);
+    }
+
+    protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToLogicalTree(e);
+        DoInitialFocus();
+    }
+
+    /// <summary>
+    /// Focus something in order to remove the focus from behind the dialog, otherwise the user may still interact with background elements using the keyboard
+    /// </summary>
+    /// <remarks>The default implementation focuses the first focusable child using DFS. If no such element was found, nothing is focused - this is undesirable behavior!</remarks>
+    protected virtual void DoInitialFocus()
+    {
+        IFocusManager? focusManager = TopLevel.GetTopLevel(this)?.FocusManager;
+        previousFocus = focusManager?.GetFocusedElement();
+        InputElement? firstFocusableDescandant = (InputElement?)UIUtil.SelectLogicalDescendant(this, x => x is InputElement inputElement && inputElement.Focusable);
+        firstFocusableDescandant?.FocusEventually();
     }
 }
 
@@ -38,29 +70,8 @@ public abstract class BaseDialog<TResult> : BaseDialog
         return new Optional<TResult>((TResult)result);
     }
 
-    public virtual void Close()
-    {
-        DialogHost.Close(null, null);
-    }
-
     protected virtual void Close(TResult result)
     {
         DialogHost.Close(null, result);
-    }
-
-    protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
-    {
-        base.OnAttachedToLogicalTree(e);
-        DoInitialFocus();
-    }
-
-    /// <summary>
-    /// Focus something in order to remove the focus from behind the dialog, otherwise the user may still interact with background elements using the keyboard
-    /// </summary>
-    /// <remarks>The default implementation focuses the first focusable child using DFS. If no such element was found, nothing is focused - this is undesirable behavior!</remarks>
-    protected virtual void DoInitialFocus()
-    {
-        InputElement? firstFocusableDescandant = (InputElement?)UIUtil.SelectLogicalDescendant(this, x => x is InputElement inputElement && inputElement.Focusable);
-        firstFocusableDescandant?.FocusEventually();
     }
 }
